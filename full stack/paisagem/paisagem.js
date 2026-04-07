3a91a2eaa0e7720c7f0f88f8a6fc740de1d02f1f
Ezequiel763190 <Ezequiel27rocha@gmail.com>
2026-04-06 22:06:41 -0300

    Add files via upload

---
+++ full stack/paisagem/paisagem.js
@@ -0,0 +1,140 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+//sol
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'yellow';
+ctx.strokeStyle = 'yellow';
+ctx.arc(330,50,30,0*Math.PI,2.5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'gray';
+ctx.strokeStyle = 'gray';
+ctx.fillRect(0,250,400,150);
+ctx.strokeRect(0,250,400,150);
+ctx.closePath();
+	
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = '#81491F';
+ctx.strokeStyle = '#81491F';
+ctx.fillRect(300,250,20,60);
+ctx.strokeRect(300,250,20,60);
+ctx.closePath();
+
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = '#81491F';
+ctx.strokeStyle = '#81491F';
+ctx.fillRect(50,188,20,60);
+ctx.strokeRect(50,188,20,60);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'green';
+ctx.strokeStyle = 'green';
+ctx.arc(60,160,30,0*Math.PI,2.5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'green';
+ctx.strokeStyle = 'green';
+ctx.arc(310,230,30,0*Math.PI,2.5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = '#81491F';
+ctx.strokeStyle = '#81491F';
+ctx.fillRect(130,118,120,130);
+ctx.strokeRect(130,118,120,130);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = '#61BBFB';
+ctx.strokeStyle = '#61BBFB';
+ctx.fillRect(138,140,40,40);
+ctx.strokeRect(138,140,40,40);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = '#61BBFB';
+ctx.strokeStyle = '#61BBFB';
+ctx.fillRect(202,140,40,40);
+ctx.strokeRect(202,140,40,40);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = '#5F4525';
+ctx.strokeStyle = '#5F4525';
+ctx.fillRect(180,180,20,68);
+ctx.strokeRect(180,180,20,68);
+ctx.closePath();
+
+// ctx.fillRect(130,118,120,130);
+ctx.beginPath();
+ctx.fillStyle = '#EC6E52';
+ctx.moveTo(130,118);
+ctx.lineTo(130+60,118-60);
+ctx.lineTo(130+120,118);
+ctx.fill();
+ctx.closePath();
+
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = '#598CFA';
+ctx.strokeStyle = '#598CFA';
+ctx.arc(0,230,40,1.5*Math.PI,0.5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = '#598CFA';
+ctx.strokeStyle = '#598CFA';
+ctx.fillRect(0,230,40,200);
+ctx.strokeRect(0,230,40,200);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = '#598CFA';
+ctx.strokeStyle = '#598CFA';
+ctx.fillRect(0,350,200,200);
+ctx.strokeRect(0,350,200,200);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = '#598CFA';
+ctx.strokeStyle = '#598CFA';
+ctx.arc(200,400,50,1*Math.PI,0*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+
+
+
+
