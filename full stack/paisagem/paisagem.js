82d9f39462522160595ff5e5db6dc7be25518cab
Ezequiel763190 <Ezequiel27rocha@gmail.com>
2026-04-11 18:01:30 -0300

    Update paisagem.js

--- full stack/paisagem/paisagem.js
+++ full stack/paisagem/paisagem.js
@@ -171,6 +171,143 @@ ctx2.closePath();
 ctx2.beginPath();
 ctx2.lineWidth = 2;
 ctx2.fillStyle = '#00ffff';
-ctx2.fillRect(357,159,40,40);
+ctx2.fillRect(2,201,40,40);
+ctx2.closePath();
+
+ctx2.beginPath();
+ctx2.lineWidth = 2;
+ctx2.fillStyle = '#00ffff';
+ctx2.fillRect(357,169,40,30);
+ctx2.closePath();
+
+ctx2.beginPath();
+ctx2.lineWidth = 2;
+ctx2.fillStyle = '#00ffff';
+ctx2.fillRect(357,201,40,30);
+ctx2.closePath();
+
+
+ctx2.beginPath();
+ctx2lineWidth = 2;
+ctx2.fillStyle = '#00eeff';
+ctx2.strokeStyle = '#1100ff';
+ctx2.arc(200,140,20,0*Math.PI,2*Math.PI);
+ctx2.fill();
+ctx2.stroke();
+ctx2.closePath();
+
+
+ctx2.beginPath();
+ctx2lineWidth = 2;
+ctx2.strokeStyle = 'green';
+ctx2.arc(200,200,100,1*Math.PI,0*Math.PI);
+ctx2.stroke();
+ctx2.closePath();
+
+
+ctx2.beginPath();
+ctx.lineWidth = 2; 
+ctx2.strokeStyle = 'blue';        
+ctx2.moveTo(2, 2);    
+ctx2.lineTo(200, 200);    
+ctx2.stroke();  
+ctx.beginPath()
+
+ctx2.beginPath();
+ctx2.lineWidth = 2; 
+ctx2.strokeStyle = 'red';        
+ctx2.moveTo(398, 2);    
+ctx2.lineTo(200, 200);    
+ctx2.stroke();  
+ctx.beginPath()
+
+ctx2.beginPath();
+ctx2.lineWidth = 2; 
+ctx2.strokeStyle = 'black';        
+ctx2.moveTo(200, 200);    
+ctx2.lineTo(200, 400);    
+ctx2.stroke();  
+ctx.beginPath()
+
+
+ctx2.beginPath();
+ctx2lineWidth = 2;
+ctx2.fillStyle = '#00eeff';
+ctx2.strokeStyle = 'green';
+ctx2.arc(200,397,40,1*Math.PI,0*Math.PI);
+ctx2.fill();
+ctx2.stroke();
+ctx2.closePath();
+
+
+ctx2.beginPath();
+ctx2lineWidth = 2;
+ctx2.strokeStyle = 'green';
+ctx2.arc(200,397,70,1.5*Math.PI,0*Math.PI);
+ctx2.stroke();
+ctx2.closePath();
+
+ctx2.beginPath();
+ctx2lineWidth = 2;
+ctx2.strokeStyle = 'green';
+ctx2.arc(200,397,90,1*Math.PI,1.5*Math.PI);
+ctx2.stroke();
+ctx2.closePath();
+
+ctx2.beginPath();
+ctx2lineWidth = 2;
+ctx2.fillStyle = 'red';
+ctx2.fillRect(144,201,55,55);
+ctx2.closePath();
+
+
+ctx2.beginPath();
+ctx2lineWidth = 2;
+ctx2.fillStyle = 'yellow';
+ctx2.strokeStyle = 'green';
+ctx2.arc(100,300,20,0*Math.PI,2*Math.PI);
+ctx2.fill();
+ctx2.stroke();
+ctx2.closePath();
+
+ctx2.beginPath();
+ctx2lineWidth = 2;
+ctx2.fillStyle = 'yellow';
+ctx2.strokeStyle = 'green';
+ctx2.arc(300,300,20,0*Math.PI,2*Math.PI);
+ctx2.fill();
+ctx2.stroke();
+ctx2.closePath();
+
+ctx2.beginPath();
+ctx2lineWidth = 2;
+ctx2.fillStyle = 'yellow';
+ctx2.fillRect(2,317,40,80);
+ctx2.closePath();
+
+ctx2.beginPath();
+ctx2lineWidth = 2;
+ctx2.fillStyle = 'yellow';
+ctx2.fillRect(40,357,40,40);
+ctx2.closePath();
+
+ctx2.beginPath();
+ctx2lineWidth = 2;
+ctx2.fillStyle = 'black';
+ctx2.fillRect(357,317,40,80);
+ctx2.closePath();
+
+ctx2.beginPath();
+ctx2lineWidth = 2;
+ctx2.fillStyle = 'black';
+ctx2.fillRect(317,357,40,40);
+ctx2.closePath();
+
+ctx2.beginPath();
+ctx2.lineWidth = 2;
+ctx2.fillStyle = 'black';
+ctx2.font = "35px Arial"
+ctx2.textAlign = "center";
+ctx2.fillText("Canvas",200,60);
 ctx2.closePath();
 
