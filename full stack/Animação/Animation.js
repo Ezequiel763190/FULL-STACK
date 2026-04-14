3c169867465308a564c122d92b5427a82fdea010
Ezequiel763190 <Ezequiel27rocha@gmail.com>
2026-04-14 15:42:14 -0300

    Update Animation.js

--- full stack/Animação/Animation.js
+++ full stack/Animação/Animation.js
@@ -1,41 +1,68 @@
 let canvas = document.getElementById('animacao');
 let ctx = canvas.getContext('2d');
 
-let bola = {
+let garoto = {
     x: 0,
-    y: 100,
-    raio: 20,
+    y: 0,
+    largura: 40,
+    altura: 60,
     img: new Image(),
     desenha: function(){
-        this.img.src = "cachorro.jpg";
         ctx.beginPath();
-        ctx.drawImage(this.img, this.x, this.y, 2*this.raio, 2*this.raio);
+        ctx.drawImage(this.img, this.x, this.y, this.largura, this.altura);
         ctx.closePath();
     }
 }
 
+garoto.img.src = "garoto.png";
+
+
+var retangulo1 = {
+    x : 0,
+    y : 0,
+    largura: 30,
+    altura: 20,
+    cor: "red",
+    desenha: function(){
+        ctx.beginPath();
+        ctx.fillStyle = this.cor;
+        ctx.fillRect(this.x, this.y, this.largura, this.altura);
+        ctx.closePath();
+    }
+}
 
 function animacao(){
-    ctx.clearRect(0,0,400,400)
-    bola.desenha();
-    requestAnimationFrame(animacao)
+    ctx.clearRect(0,0,400,400);
+    if (retangulo1.x >= 270){
+        aux = -1;
+    }
+
+    if (retangulo1.x <= 0){
+        aux = 1
+    }
+
+    retangulo1.x = retangulo1.x + aux*3;
+    retangulo1.desenha();
+    garoto.desenha();
+    requestAnimationFrame(animacao);
 }
+
 animacao();
+
 document.addEventListener('mousemove', function(evento){
     let rect = canvas.getBoundingClientRect();
     let x_mouse = evento.clientX - rect.left;
     let y_mouse = evento.clientY - rect.top;
 
+    garoto.x = x_mouse - garoto.largura / 2;
+    garoto.y = y_mouse - garoto.altura / 2;
 
-    bola.x = x_mouse - bola.raio;
-    bola.y = y_mouse - bola.raio;
+    if (garoto.x < 0) garoto.x = 0;
+    if (garoto.y < 0) garoto.y = 0;
 
-    if (bola.x < 0) bola.x = 0;
-    if (bola.y < 0) bola.y = 0;
+    if (garoto.x + garoto.largura > canvas.width)
+        garoto.x = canvas.width - garoto.largura;
 
-    if (bola.x + 2*bola.raio > canvas.width)
-        bola.x = canvas.width - 2*bola.raio;
-
-    if (bola.y + 2*bola.raio > canvas.height)
-        bola.y = canvas.height - 2*bola.raio;
+    if (garoto.y + garoto.altura > canvas.height)
+        garoto.y = canvas.height - garoto.altura;
 });
